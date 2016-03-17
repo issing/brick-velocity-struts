da813d9ea57a56543f0168e31982c98c0208e4b4
issing <issing@isger.net>
2016-03-17 16:33:16 +0800

    初始版本

---
+++ src/main/java/net/isger/brick/velocity/struts/VelocityManager.java
@@ -0,0 +1,90 @@
+package net.isger.brick.velocity.struts;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+import javax.servlet.ServletContext;
+
+import net.isger.brick.velocity.VelocityConstants;
+import net.isger.brick.velocity.directive.DirectiveLibrary;
+
+import com.opensymphony.xwork2.inject.Container;
+
+public class VelocityManager extends
+        org.apache.struts2.views.velocity.VelocityManager implements
+        VelocityConstants {
+
+    private static final String KEY_FILE_PATH = "strutsfile.resource.loader.path";
+
+    private static final String KEY_DIRECTIVE = "userdirective";
+
+    private List<DirectiveLibrary> libraries;
+
+    public void setContainer(Container container) {
+        super.setContainer(container);
+        List<DirectiveLibrary> list = new ArrayList<DirectiveLibrary>();
+        Set<String> prefixes = container
+                .getInstanceNames(DirectiveLibrary.class);
+        for (String prefix : prefixes) {
+            list.add(container.getInstance(DirectiveLibrary.class, prefix));
+        }
+        this.libraries = Collections.unmodifiableList(list);
+    }
+
+    public Properties loadConfiguration(ServletContext context) {
+        // 模板加载路径集
+        Properties props = super.loadConfiguration(context);
+        String paths = props.getProperty(KEY_FILE_PATH);
+        if (paths != null) {
+            props.setProperty(KEY_FILE_PATH, transPath(context, paths));
+        }
+        // 自定义砖头指令集
+        StringBuffer buffer = new StringBuffer(512);
+        for (DirectiveLibrary library : this.libraries) {
+            List<Class<?>> directives = library.getDirectiveClasses();
+            if (directives != null) {
+                for (Class<?> d : directives) {
+                    append(buffer, d.getName());
+                }
+            }
+        }
+        props.setProperty(KEY_DIRECTIVE,
+                buffer.append(props.getProperty(KEY_DIRECTIVE)).toString());
+        // 初始默认配置
+        initProperty(props, KEY_LAYOUT_PATH, LAYOUT_PATH);
+        initProperty(props, KEY_LAYOUT_NAME, LAYOUT_NAME);
+        initProperty(props, KEY_THEME_NAME, THEME_NAME);
+        initProperty(props, KEY_WIDGET_PATH, WIDGET_PATH);
+        return props;
+    }
+
+    private void initProperty(Properties props, String key, String def) {
+        String val = props.getProperty(key);
+        if (val == null || val.trim().length() == 0) {
+            props.setProperty(key, def);
+        }
+    }
+
+    private String transPath(ServletContext context, String paths) {
+        StringBuffer buffer = new StringBuffer(512);
+        StringTokenizer st = new StringTokenizer(paths, ",");
+        String path;
+        while (st.hasMoreTokens()) {
+            path = st.nextToken();
+            if (!new File(path).isAbsolute()) {
+                append(buffer, context.getRealPath(path));
+            }
+        }
+        return buffer.toString();
+    }
+
+    private void append(StringBuffer buffer, String value) {
+        buffer.append(value).append(", ");
+    }
+
+}
---
+++ src/main/java/net/isger/brick/velocity/struts/VelocityResult.java
@@ -0,0 +1,214 @@
+package net.isger.brick.velocity.struts;
+
+import java.io.StringWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.isger.brick.velocity.ContextSecretary;
+import net.isger.brick.velocity.VelocityConstants;
+import net.isger.brick.velocity.VelocityContext;
+import net.isger.brick.velocity.bean.LayoutBean;
+import net.isger.brick.velocity.bean.ThemeBean;
+import net.isger.util.Strings;
+
+import org.apache.struts2.views.velocity.VelocityManager;
+import org.apache.velocity.Template;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.context.Context;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.util.ValueStack;
+
+public class VelocityResult extends
+        org.apache.struts2.dispatcher.VelocityResult implements
+        VelocityConstants {
+
+    private static final long serialVersionUID = -3630891950185638015L;
+
+    private ActionInvocation invocation;
+
+    private Template template;
+
+    private String velocityIndex;
+
+    private String velocitySuffix;
+
+    private String contentType;
+
+    private String target;
+
+    private String themePath;
+
+    private String themeName;
+
+    private String layoutSupport;
+
+    private String layoutPath;
+
+    private String layoutName;
+
+    private ThemeBean theme;
+
+    private LayoutBean layout;
+
+    public void doExecute(String finalLocation, ActionInvocation invocation)
+            throws Exception {
+        this.invocation = invocation;
+        super.doExecute(finalLocation, invocation);
+    }
+
+    protected String getContentType(String templateLocation) {
+        if (contentType == null) {
+            contentType = super.getContentType(templateLocation);
+        } else {
+            contentType = conditionalParse(contentType, invocation);
+        }
+        return contentType;
+    }
+
+    protected Template getTemplate(ValueStack stack, VelocityEngine velocity,
+            ActionInvocation invocation, String location, String encoding)
+            throws Exception {
+        this.invocation = invocation;
+        String velocitySuffix = getProperty(stack, velocity,
+                KEY_VELOCITY_SUFFIX, this.velocitySuffix, VELOCITY_SUFFIX);
+        String target = conditionalParse(this.target, invocation);
+        if (Strings.isNotEmpty(target)) {
+            location += "/" + target;
+        } else if (location.endsWith("/")) {
+            location += getProperty(stack, velocity, KEY_VELOCITY_INDEX,
+                    this.velocityIndex, VELOCITY_INDEX);
+        }
+        location += velocitySuffix;
+        // 获取主题信息
+        this.theme = new ThemeBean();
+        this.theme.setPath(getProperty(stack, velocity, KEY_THEME_PATH,
+                this.themePath, THEME_PATH));
+        this.theme.setName(getProperty(stack, velocity, KEY_THEME_NAME,
+                this.themeName, THEME_NAME));
+        this.theme.setLocation(location);
+        // 检查布局信息
+        this.layout = new LayoutBean();
+        this.layout.setSupport(Boolean.parseBoolean(getProperty(stack,
+                velocity, KEY_LAYOUT_SUPPORT, this.layoutSupport,
+                LAYOUT_SUPPORT)));
+        this.layout.setPath(getProperty(stack, velocity, KEY_LAYOUT_PATH,
+                this.layoutPath, LAYOUT_PATH));
+        this.layout.setName(getProperty(stack, velocity, KEY_LAYOUT_NAME,
+                this.layoutName, LAYOUT_NAME) + velocitySuffix);
+        if (this.layout.isSupport()) {
+            // 提取内容页面
+            this.template = super.getTemplate(stack, velocity, invocation,
+                    this.theme.getLocation(), encoding);
+            // 获取布局参数
+            StringBuffer buffer = new StringBuffer(128);
+            buffer.append(this.layout.getPath());
+            buffer.append("/").append(this.theme.getName());
+            buffer.append("/").append(this.layout.getName());
+            location = buffer.toString();
+        } else {
+            location = this.theme.getLocation();
+        }
+        // 提取目标模板（布局/内容）
+        return super.getTemplate(stack, velocity, invocation, location,
+                encoding);
+    }
+
+    protected Context createContext(VelocityManager velocityManager,
+            ValueStack stack, HttpServletRequest request,
+            HttpServletResponse response, String location) {
+        VelocityContext context = new VelocityContext(
+                velocityManager.getVelocityEngine(), super.createContext(
+                        velocityManager, stack, request, response, location));
+        ContextSecretary secretary = context.getSecretary();
+        secretary.setTheme(theme);
+        secretary.setLayout(layout);
+        if (this.template != null) {
+            // 生成布局模板内容
+            StringWriter sw = new StringWriter();
+            try {
+                this.template.merge(context, sw);
+            } catch (Exception e) {
+                throw new IllegalStateException(e.getMessage(), e.getCause());
+            }
+            layout.setScreen(sw.toString());
+        }
+        return context;
+    }
+
+    public void setVelocityIndex(String velocityIndex) {
+        this.velocityIndex = velocityIndex;
+    }
+
+    public void setVelocitySuffix(String velocitySuffix) {
+        this.velocitySuffix = velocitySuffix;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    /**
+     * 主题属性
+     * 
+     * @return
+     */
+    public void setThemePath(String themePath) {
+        this.themePath = themePath;
+    }
+
+    public void setThemeName(String themeName) {
+        this.themeName = themeName;
+    }
+
+    /**
+     * 布局属性
+     * 
+     * @return
+     */
+    public void setLayoutSupport(String layoutSupport) {
+        this.layoutSupport = layoutSupport;
+    }
+
+    public void setLayoutPath(String layoutPath) {
+        this.layoutPath = layoutPath;
+    }
+
+    public void setLayoutName(String layoutName) {
+        this.layoutName = layoutName;
+    }
+
+    /**
+     * 获取配置属性
+     * 
+     * @param stack
+     * @param velocity
+     * @param invocation
+     * @param key
+     * @param local
+     * @param def
+     * @return
+     */
+    private String getProperty(ValueStack stack, VelocityEngine velocity,
+            String key, String local, String def) {
+        String result = stack.findString(key);
+        if (Strings.isNotEmpty(result)) {
+            return result;
+        }
+        result = conditionalParse(local, invocation);
+        if (Strings.isNotEmpty(result)) {
+            return result;
+        }
+        Object value = velocity.getProperty(key);
+        if (value != null && Strings.isNotEmpty(local = value.toString())) {
+            def = local;
+        }
+        return def;
+    }
+
+}
